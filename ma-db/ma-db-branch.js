08e5da25dc7c06e2b5b5106cdf808c31bba79703
Maxim Kupfer <MKupfer@staffingindustry.com>
2018-04-08 22:15:12 -0700

    Upload branch of ma-db.js

---
+++ ma-db/ma-db-branch.js
@@ -0,0 +1,60 @@
+var table = document.querySelector('tbody')
+var requestURL = 'https://mbkupfer.github.io/data/madb.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function(){
+  var tracker = request.response;
+  populateTable(tracker);
+  var options = {
+    valueNames: [ 'quarter', 'region', 'acquirer', 'targetSegment', 'hyperlink']
+  };
+
+  var transactionList = new List('database', options);
+  $(".filter").keyup(function(){
+    transactionList.filter(item => {
+      return ['region', 'acquirer', 'targetSegment'].every(name => {
+        var value = $("#"+name).val().toLowerCase();
+        return item.values()[name].toLowerCase().includes(value)
+      });
+    });
+  })
+}
+
+function populateTable(jsonObj){
+  var transactions = jsonObj['transactions'];
+  for(var i = 0; i < transactions.length; i++){
+    var row = document.createElement('tr');
+    var quarter = document.createElement('td');
+    quarter.className = 'quarter';
+    var region = document.createElement('td');
+    region.className = 'region';
+    var acquirer = document.createElement('td');
+    acquirer.className = 'acquirer';
+    var targetSegment = document.createElement('td');
+    targetSegment.className = 'targetSegment';
+    var hyperlink = document.createElement('td');
+    hyperlink.className = 'hyperlink';
+
+    quarterText = document.createTextNode(transactions[i].quarter);
+    regionText =  document.createTextNode(transactions[i].region);
+    acquirerText = document.createTextNode(transactions[i].acquirer);
+    targetSegmentText = document.createTextNode(transactions[i].targetSegment);
+    hyperlinkText = document.createTextNode(transactions[i].hyperlink);
+
+
+    quarter.appendChild(quarterText);
+    region.appendChild(regionText);
+    acquirer.appendChild(acquirerText);
+    targetSegment.appendChild(targetSegmentText);
+    hyperlink.appendChild(hyperlinkText);
+
+    row.appendChild(quarter);
+    row.appendChild(region);
+    row.appendChild(acquirer);
+    row.appendChild(targetSegment);
+    row.appendChild(hyperlink);
+    table.appendChild(row);
+  }
+}
